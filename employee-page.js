97df8ac528d8f1135d9e8a4a86f8eb665aa287fd
Aviral09 <aviralgoel2001@gmail.com>
2020-07-12 20:44:43 +0530

    Employee details

---
+++ employee-page.js
@@ -0,0 +1,43 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDiAD9-E0JkSKt3BD5y5Gpj7OP5Cz1ZSbc",
+    authDomain: "officemanagement-3196a.firebaseapp.com",
+    databaseURL: "https://officemanagement-3196a.firebaseio.com",
+    projectId: "officemanagement-3196a",
+    storageBucket: "officemanagement-3196a.appspot.com",
+    messagingSenderId: "810522578256",
+    appId: "1:810522578256:web:6a05bc912515494b26f055",
+    measurementId: "G-ELCBHR461S"
+  };
+if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
+
+
+var db = firebase.firestore()
+var employees = []
+
+db.collection("employee").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+        employees.push(doc.data())
+        console.log(doc.data())
+    });
+});
+
+//use employees to get data of employees
+console.log(employees)
+
+function Emp(employees){
+    return `
+    <div class="card " style="width: 18rem; ">
+                    <img class="card-img-top " src="./images/office.jpg" alt="Card image cap ">
+                    <div class="card-body ">
+                        <h5 class="card-title ">${employees.name}</h5>
+                        <p class="card-text ">Card information</p>
+                        <a href="# " class="btn btn-primary ">Go somewhere</a>
+                    </div>
+    </div>hello
+    `
+
+}
+
+document.getElementById("insert").innerHTML = `
+    ${employees.map(Emp).join('')}
+`
